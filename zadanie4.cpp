78c43047c2520dcfcdfd4cb8e08a5a236b12f562
Anna <anna.wawrzynow@gmail.com>
2016-06-08 21:43:33 +0200

    zadanie
    zadanie

--- zadanie4.cpp
+++ zadanie4.cpp
@@ -2,8 +2,32 @@
 #include <string>
 #include <fstream> // biblioteka do wczytywania pliku
 #include <vector>
+#include <conio.h>
 using namespace std;
 
+class human
+{
+public:
+    string name;
+    string surname;
+    string number;
+    human( string xname, string xsurname, string xnumber );
+};
+
+void funkcja_open () // ta funkja dzia³a tylko po³owicznie - nie wypisuje danych z pliku tylko podaje komunikat z else
+{
+	string linia;
+	ifstream uchwyt;
+	if (uchwyt.is_open()) //otwieramy plik
+						{
+						while (getline(uchwyt,linia))
+							{
+								cout<< linia << endl;
+							}
+					uchwyt.close ();
+											}
+				else cout << "Unable to open file"<<'\n'; 	
+}
 void funkcja_catalog () //funkcja wczytuje pliki 
 {
 	string name;
@@ -17,36 +41,40 @@ void funkcja_catalog () //funkcja wczytuje pliki
 			{
 					if(name=="author")
 					{
-					string linia;
-				 ifstream uchwyt ("dane.txt"); //obiekt typu fstream (uchwyt do pliku) 
-   					 if (uchwyt.is_open()) //otwieramy plik
+					ifstream uchwyt ("dane.txt");
+					funkcja_open (); 						// nie dzia³a dla while 
+				 /*obiekt typu fstream (uchwyt do pliku) 
+				 if (uchwyt.is_open()) //otwieramy plik
 						{
 						while (getline(uchwyt,linia))
 							{
 								cout<< linia << endl;
 							}
-					uchwyt.close ();	
-						}
-					else cout << "Unable to open file"; 	
-					}
+					uchwyt.close (); */
+					}                                         // niestety funkcja open nie otwiera pliku
+				//else cout << "Unable to open file"; 	
+				//}
 					
         			else if(name=="title")
            			{
 					string linia;
 				 ifstream uchwyt ("katalog.txt"); //obiekt typu fstream (uchwyt do pliku) 
-   					 if (uchwyt.is_open()) //otwieramy plik
+   				
+			   	 if (uchwyt.is_open()) //otwieramy plik
 						{
 						while (getline(uchwyt,linia))
 							{
 								cout<< linia << endl;
 							}
 					uchwyt.close ();	
+			
 						}
-					else cout << "Unable to open file"; 	
+					else cout << "Unable to open file"; 
 					} 
 				else if (name=="quit")
 				{
 			cout <<"Goodbye" << endl;
+			
 			}
 			else cout <<"Choose" << endl;
 						
@@ -55,8 +83,45 @@ void funkcja_catalog () //funkcja wczytuje pliki
 
 void funkcja_help ()
 {
-	cout <<"Go to help." << endl; 
+vector < human > osoba;
+    cout << "Baza Osob\n1.-Lista osob\n2.-Dodaj osobe\n3.-Wyjscie";
+    char odp;
+    do
+    {
+        odp = getch();
+        switch( odp )
+        {
+        case '1':
+           cout << "\n\n\nLista osob:\n";				// wypisuje tylko wpisane dane , po zamkniêciu i otworzeniu programu dane ulegaj¹ zniszczeniu
+            for( int i = 0; i < osoba.size(); i++ )
+            {
+                cout << endl;
+                cout << "Name: " << osoba[ i ].name << endl;
+                cout << "Surname: " << osoba[ i ].surname << endl;
+                cout << "Number library card: " << osoba[ i ].number << endl;
+            } 
+            break;
+        case '2': 			// nie wiem, jak zapisaæ do pliku - czy tutaj trzeba otworzyæ nowy plik?
+		{
+			cout << "\n\n\n|----DODAWANIE_OSOBY_DO_LISTY-----|";
+            cout << "\nName: ";
+            string odp_name;
+            cin >> odp_name;
+            cout << "Surname: ";
+            string odp_surname;
+            cin >> odp_surname;
+            cout << "Number library card: ";
+            string odp_number;
+            cin >> odp_number;
+            osoba.push_back( human( odp_name, odp_surname, odp_number ) );
+            cout << "\n<***ZAKONCZONO_Z_SUKCESEM***>";
+    	}
+            break;
+        }
+    }
+    while( odp != '3' );	
 }
+			
 
 int main() 
 {    
@@ -76,12 +141,11 @@ int main()
 
 	cin>> decision;
 
-	switch (decision)	
+switch (decision)	
 	{	
 		case 1:
 			funkcja_catalog ();
-		break;
-	
+			break;
 		case 2:
 			funkcja_help ();
 			break;
@@ -89,10 +153,66 @@ int main()
 			cout << "Goodbye." << endl; break;
 		default:
 			cout << "I don't undersand. Choose 1, 2 or 3." << endl;
-	}	
+
+	}
 	
 
-	/*----------- to dopiero rozgryzam */
+
+while (decision>3)
+	{
+	cin>> decision;
+		switch (decision)	
+	{
+		case 1:
+			funkcja_catalog ();
+			break;
+		case 2:
+			funkcja_help ();
+			break;
+		case 3:
+			cout << "Goodbye." << endl; break;
+		default:
+			cout << "I don't undersand. Choose 1, 2 or 3." << endl;
+	}
+			} 
+			cout<< "/---------------------------------------------/"<< endl;
+
+
+/* tu wklejam moje próby z tworzeniem pliku i wprowadzaniem 1 danej*/
+ cout<< "napisz imiê" << endl;
+// cin>>name;     - to z kolei nie dopisuje do pliku            
+	fstream katalog;
+	katalog.open ("ja.txt", ios::out|ios::app);
+	getline (cin, name);
+	katalog<<name << '\n';  // jako osobny plik pozwala wpisywaæ imiona atutaj nie
+	katalog.close ();
+
+ { 
+ string linia;
+ 	ifstream katalog;
+ katalog.open("ja.txt", ios::in); // otwarcie pliku
+ if(katalog.good()) // sprawdzamy czy plik istnieje
+{
+ 	while(!katalog.eof())
+ 	{
+ 		getline(katalog, linia);
+ 		cout<< linia<< '\n';
+	 }
+}
+ if(katalog==NULL)   // nie wiem dlaczego to te¿ siê wykonuje na ekranie
+ {
+ 	cout<<"plik jest pusty"; 	
+	katalog.close();
+ }
+else cout<<"plik nie istnieje";
+}
+ getch();
+ 
+ 
+ /*----------- to dopiero rozgryzam */
+		
+		
+		
 			// stwórz w3asn1 funkcje w której wczytasz katalog 
 			// i tutaj j1 wywo3aj - kod bedzie czytelniejszy 
 			
@@ -101,8 +221,7 @@ int main()
 			// podpowiedzi szukaj w cPlusPlusMaterials\Kod\Arrays&Functions
 			// i plik functions.cpp i jeszcze w webinarium o wektorze
 			// np:
-	//	string file = "katalog.txt";
-	//	vector<string> catalog = read_catalog(file);
+	
 			// zamiast wektora mo?esz u?ya oczywiocie normalnych tablic ;)
 			// ale z wektorem mo?e bya proociej ;)
 	//		cout << "Welcome to the catalog." << endl; break;
@@ -115,25 +234,12 @@ int main()
 			
 			// jeoli nie bedziesz mia3a pomys3u co z tym zrobia to 
 			// pisz, a ja moge pomóc coo zaimplementowaa! 
-
-
-while (decision>3)
-	{
-	cin>> decision;
-		switch (decision)	
-	{
-		case 1:
-			funkcja_catalog ();
-			 break;
-		case 2:
-			funkcja_help ();
-			break;
-		case 3:
-			cout << "Goodbye." << endl; break;
-		default:
-			cout << "I don't undersand. Choose 1, 2 or 3." << endl;
-	}
-			} 
+			
 	return 0;
 }
 
+
+human::human( string xname, string xsurname, string xnumber )
+  : name( xname ), surname( xsurname ), number( xnumber )
+  {
+ }
